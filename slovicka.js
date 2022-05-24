db364359386cf6fb8b2ab84d9e3b618be147adb7
alicter <85372451+alicter@users.noreply.github.com>
2022-05-24 07:59:35 +0200

    Add files via upload

---
+++ slovicka.js
@@ -0,0 +1,1105 @@
+a = new Array;
+b = new Array;
+d = new Array;
+e = new Array;
+
+a[a.length] = "uhasit";
+b[b.length] = "extinguish";
+
+a[a.length] = "neochotný";
+b[b.length] = "unwilling";
+
+a[a.length] = "luk";
+b[b.length] = "bow";
+
+a[a.length] = "svázaný";
+b[b.length] = "banded";
+
+a[a.length] = "vzdor";
+b[b.length] = "defiance";
+
+a[a.length] = "porušující";
+b[b.length] = "violating";
+
+a[a.length] = "přísaha";
+b[b.length] = "oath";
+
+a[a.length] = "kapitulace";
+b[b.length] = "surrender";
+
+a[a.length] = "předchozí";
+b[b.length] = "prior";
+
+a[a.length] = "představil";
+b[b.length] = "staged";
+
+a[a.length] = "vzpruha";
+b[b.length] = "upspring";
+
+a[a.length] = "posádka";
+b[b.length] = "garrison";
+
+a[a.length] = "angažovanost";
+b[b.length] = "engagement";
+
+a[a.length] = "doručit";
+b[b.length] = "deliver";
+
+a[a.length] = "nad hlavou";
+b[b.length] = "overhead";
+
+a[a.length] = "zoufalý";
+b[b.length] = "desperate";
+
+a[a.length] = "mimo";
+b[b.length] = "beyond";
+
+a[a.length] = "vpřed";
+b[b.length] = "forward";
+
+a[a.length] = "řev";
+b[b.length] = "roar";
+
+a[a.length] = "držený";
+b[b.length] = "held";
+
+a[a.length] = "říše";
+b[b.length] = "realm";
+
+a[a.length] = "rámus";
+b[b.length] = "din";
+
+a[a.length] = "strana";
+b[b.length] = "party";
+
+a[a.length] = "schopný";
+b[b.length] = "capable";
+
+a[a.length] = "získat";
+b[b.length] = "acquire";
+
+a[a.length] = "útes";
+b[b.length] = "cliff";
+
+a[a.length] = "rokle";
+b[b.length] = "ravine";
+
+a[a.length] = "stew";
+b[b.length] = "naložit";
+
+a[a.length] = "zjistit";
+b[b.length] = "find out";
+
+a[a.length] = "strmý";
+b[b.length] = "precipitous";
+
+a[a.length] = "špíz";
+b[b.length] = "skewer";
+
+a[a.length] = "napadnout";
+b[b.length] = "invade";
+
+a[a.length] = "rozkoš";
+b[b.length] = "delight";
+
+a[a.length] = "návod";
+b[b.length] = "walkthrough";
+
+a[a.length] = "vysoký";
+b[b.length] = "tall";
+
+a[a.length] = "lesson";
+b[b.length] = "lekce";
+
+a[a.length] = "konstruktér";
+b[b.length] = "designer";
+
+a[a.length] = "jistě";
+b[b.length] = "certainly";
+
+a[a.length] = "hrát";
+b[b.length] = "play";
+
+a[a.length] = "svetr";
+b[b.length] = "pullover";
+
+a[a.length] = "podél";
+b[b.length] = "along";
+
+a[a.length] = "cizí";
+b[b.length] = "foreign";
+
+a[a.length] = "čtvrť";
+b[b.length] = "quarter";
+
+a[a.length] = "obvyklý program";
+b[b.length] = "routine";
+
+a[a.length] = "zřídka";
+b[b.length] = "seldom";
+
+a[a.length] = "minulost";
+b[b.length] = "past";
+
+a[a.length] = "plot";
+b[b.length] = "fence";
+
+a[a.length] = "pokrok";
+b[b.length] = "progress";
+
+a[a.length] = "aktuální";
+b[b.length] = "up to date";
+
+a[a.length] = "načrtnout";
+b[b.length] = "outline";
+
+a[a.length] = "odhad";
+b[b.length] = "estimate";
+
+a[a.length] = "zda";
+b[b.length] = "whether";
+
+a[a.length] = "odrůda";
+b[b.length] = "variety";
+
+a[a.length] = "hodnota";
+b[b.length] = "worth";
+
+a[a.length] = "dále";
+b[b.length] = "forth";
+
+a[a.length] = "ty sám";
+b[b.length] = "thyself";
+
+a[a.length] = "reagovat";
+b[b.length] = "respond";
+
+a[a.length] = "proti";
+b[b.length] = "against";
+
+a[a.length] = "z ruky";
+b[b.length] = "out of hand";
+
+a[a.length] = "symfonický";
+b[b.length] = "symphonic";
+
+a[a.length] = "eklektický";
+b[b.length] = "eclectic";
+
+a[a.length] = "zplodil";
+b[b.length] = "begotten";
+
+a[a.length] = "převrácený";
+b[b.length] = "upended";
+
+a[a.length] = "přepaden";
+b[b.length] = "rauaged";
+
+a[a.length] = "průchod";
+b[b.length] = "passage";
+
+a[a.length] = "zabitý";
+b[b.length] = "slain";
+
+a[a.length] = "zbaveno";
+b[b.length] = "seized";
+
+a[a.length] = "výkupné";
+b[b.length] = "ransom";
+
+a[a.length] = "doutnat";
+b[b.length] = "smolder";
+
+a[a.length] = "spravedlivý";
+b[b.length] = "righteous";
+
+a[a.length] = "třírohý";
+b[b.length] = "cornered";
+
+a[a.length] = "rozkoš";
+b[b.length] = "delight";
+
+a[a.length] = "aktuální";
+b[b.length] = "current";
+
+a[a.length] = "proud";
+b[b.length] = "current";
+
+a[a.length] = "částka";
+b[b.length] = "amount";
+
+a[a.length] = "docela";
+b[b.length] = "quite";
+
+a[a.length] = "dodatečný";
+b[b.length] = "additional";
+
+a[a.length] = "klečet";
+b[b.length] = "kneel";
+
+a[a.length] = "poháněno";
+b[b.length] = "powered by";
+
+a[a.length] = "vylepšené";
+b[b.length] = "enhanced";
+
+a[a.length] = "rovnat se";
+b[b.length] = "equal";
+
+a[a.length] = "mimo";
+b[b.length] = "out of";
+
+a[a.length] = "konsolidované";
+b[b.length] = "consolidated";
+
+a[a.length] = "celý";
+b[b.length] = "entire";
+
+a[a.length] = "vydání";
+b[b.length] = "issue";
+
+a[a.length] = "proklamace";
+b[b.length] = "proclamation";
+
+a[a.length] = "neštovice";
+b[b.length] = "pox";
+
+a[a.length] = "roční období";
+b[b.length] = "season";
+
+a[a.length] = "jáma";
+b[b.length] = "pit";
+
+a[a.length] = "upravený";
+b[b.length] = "groomed";
+
+a[a.length] = "jasný";
+b[b.length] = "bright";
+
+a[a.length] = "překvapující";
+b[b.length] = "striking";
+
+a[a.length] = "odporný";
+b[b.length] = "vile";
+
+a[a.length] = "vířit se";
+b[b.length] = "swirl";
+
+a[a.length] = "kostrbatý";
+b[b.length] = "gritty";
+
+a[a.length] = "chodba";
+b[b.length] = "hallway";
+
+a[a.length] = "zelí";
+b[b.length] = "cabbage";
+
+a[a.length] = "rag";
+b[b.length] = "hadr";
+
+a[a.length] = "mats";
+b[b.length] = "podložky";
+
+a[a.length] = "ability";
+b[b.length] = "schopnost";
+
+a[a.length] = "order of combat";
+b[b.length] = "bojový řád";
+
+a[a.length] = "cover";
+b[b.length] = "krýt";
+
+a[a.length] = "reward";
+b[b.length] = "odměna";
+
+a[a.length] = "casting";
+b[b.length] = "vrhání";
+
+a[a.length] = "crumbling";
+b[b.length] = "rozpadající se";
+
+
+a[a.length] = "limitless";
+b[b.length] = "bezmezný";
+
+a[a.length] = "dungeon";
+b[b.length] = "kobka";
+
+a[a.length] = "among";
+b[b.length] = "mezi";
+
+a[a.length] = "master";
+b[b.length] = "pán";
+
+a[a.length] = "trees";
+b[b.length] = "stromy";
+
+a[a.length] = "round";
+b[b.length] = "zaoblený";
+
+a[a.length] = "jut";
+b[b.length] = "vyčnívat";
+
+a[a.length] = "archway";
+b[b.length] = "klenutá chodba";
+
+a[a.length] = "littered";
+b[b.length] = "hemžící se";
+
+a[a.length] = "twisted";
+b[b.length] = "zvrácený";
+
+
+a[a.length] = "shard";
+b[b.length] = "úlomky";
+
+a[a.length] = "rust";
+b[b.length] = "koroze";
+
+
+a[a.length] = "attainable";
+b[b.length] = "dosažitelný";
+
+a[a.length] = "fittingly";
+b[b.length] = "vhodně";
+
+a[a.length] = "proposed";
+b[b.length] = "navržený";
+
+a[a.length] = "mark";
+b[b.length] = "označit";
+
+a[a.length] = "levity";
+b[b.length] = "lehkomyslnost";
+
+a[a.length] = "assent";
+b[b.length] = "souhlas";
+
+a[a.length] = "mode";
+b[b.length] = "režim";
+
+a[a.length] = "procedure";
+b[b.length] = "postup";
+
+a[a.length] = "refutation";
+b[b.length] = "vyvrácení";
+
+
+a[a.length] = "foregoing";
+b[b.length] = "výše uvedené";
+
+a[a.length] = "soulution";
+b[b.length] = "řešení";
+
+a[a.length] = "aforesaid";
+b[b.length] = "výše řečeno";
+
+a[a.length] = "remotion";
+b[b.length] = "oddálení";
+
+a[a.length] = "eternal";
+b[b.length] = "věčný";
+
+a[a.length] = "matter";
+b[b.length] = "hmota";
+
+a[a.length] = "violent";
+b[b.length] = "násilný";
+
+a[a.length] = "beside";
+b[b.length] = "vedle";
+
+a[a.length] = "genus";
+b[b.length] = "rod";
+
+a[a.length] = "formal";
+b[b.length] = "formální";
+
+a[a.length] = "likeness";
+b[b.length] = "podoba";
+
+a[a.length] = "terms";
+b[b.length] = "podmínky";
+
+a[a.length] = "inconsistent with";
+b[b.length] = "v rozsporu s";
+
+a[a.length] = "proper";
+b[b.length] = "správné";
+
+a[a.length] = "inquiring";
+b[b.length] = "tázavý";
+
+a[a.length] = "multitude";
+b[b.length] = "zástup";
+
+a[a.length] = "division";
+b[b.length] = "oddělení";
+
+a[a.length] = "enunciation";
+b[b.length] = "výslovnost";
+
+a[a.length] = "supreme";
+b[b.length] = "nejvyšší";
+
+a[a.length] = "deny";
+b[b.length] = "odmítnout";
+
+a[a.length] = "contingent";
+b[b.length] = "závisející";
+
+a[a.length] = "willing";
+b[b.length] = "ochotný";
+
+a[a.length] = "supposition";
+b[b.length] = "předpoklad";
+
+a[a.length] = "assign";
+b[b.length] = "přidělit";
+
+a[a.length] = "appetite";
+b[b.length] = "hlad";
+
+a[a.length] = "delight";
+b[b.length] = "požitek";
+
+a[a.length] = "nor";
+b[b.length] = "ani";
+
+a[a.length] = "incompatible";
+b[b.length] = "nekompatibilní";
+
+a[a.length] = "ascribed";
+b[b.length] = "připsal";
+
+a[a.length] = "sequel";
+b[b.length] = "pokračování";
+
+a[a.length] = "consideration";
+b[b.length] = "ohleduplnost";
+
+a[a.length] = "avail";
+b[b.length] = "využití";
+
+a[a.length] = "refute";
+b[b.length] = "vyvracet";
+
+a[a.length] = "treat";
+b[b.length] = "zacházet";
+
+a[a.length] = "aforesaid";
+b[b.length] = "výše řečeno";
+
+a[a.length] = "succession";
+b[b.length] = "posloupnost";
+
+a[a.length] = "unable";
+b[b.length] = "neschopný";
+
+a[a.length] = "certain";
+b[b.length] = "jistý";
+
+a[a.length] = "due";
+b[b.length] = "z důvodu";
+
+a[a.length] = "endeavor";
+b[b.length] = "usilovat";
+
+a[a.length] = "standpoint";
+b[b.length] = "stanovisko";
+
+a[a.length] = "distinctions";
+b[b.length] = "rozdíly";
+
+a[a.length] = "contrariety";
+b[b.length] = "rozpor";
+
+a[a.length] = "agents";
+b[b.length] = "zástupci";
+
+a[a.length] = "diversity";
+b[b.length] = "rozmanitost";
+
+a[a.length] = "merits";
+b[b.length] = "zásluhy";
+
+a[a.length] = "demerits";
+b[b.length] = "nedostatky";
+
+a[a.length] = "capable";
+b[b.length] = "schopný";
+
+a[a.length] = "incorruptible";
+b[b.length] = "neúplatný";
+
+a[a.length] = "nutritive";
+b[b.length] = "výživný";
+
+a[a.length] = "sensitive";
+b[b.length] = "citlivý";
+
+a[a.length] = "intellective";
+b[b.length] = "rozumový";
+
+a[a.length] = "faculties";
+b[b.length] = "nadání";
+
+a[a.length] = "derive";
+b[b.length] = "odvodit";
+
+a[a.length] = "aforesaid";
+b[b.length] = "výše řečeno";
+
+a[a.length] = "harmony";
+b[b.length] = "harmonie";
+
+a[a.length] = "thereof";
+b[b.length] = "z toho";
+
+a[a.length] = "assert";
+b[b.length] = "tvrdit";
+
+a[a.length] = "concur";
+b[b.length] = "souhlasit";
+
+a[a.length] = "concern";
+b[b.length] = "znepokojení";
+
+a[a.length] = "dumb";
+b[b.length] = "němý";
+
+a[a.length] = "transmit";
+b[b.length] = "přenášet";
+
+a[a.length] = "semen";
+b[b.length] = "sperma";
+
+a[a.length] = "proud";
+b[b.length] = "hrdý";
+
+a[a.length] = "impiety";
+b[b.length] = "bezbožnost";
+
+a[a.length] = "naming";
+b[b.length] = "pojmenování";
+
+a[a.length] = "hence";
+b[b.length] = "proto";
+
+a[a.length] = "conceive";
+b[b.length] = "otěhotnět";
+
+a[a.length] = "reckons";
+b[b.length] = "počítá";
+
+a[a.length] = "direct";
+b[b.length] = "přímo";
+
+a[a.length] = "government";
+b[b.length] = "vláda";
+
+a[a.length] = "disposed";
+b[b.length] = "zlikvidován";
+
+a[a.length] = "druggist";
+b[b.length] = "lékárník";
+
+a[a.length] = "equestrian";
+b[b.length] = "jezdecký";
+
+a[a.length] = "warlike";
+b[b.length] = "válečný";
+
+a[a.length] = "appliance";
+b[b.length] = "spotřebič";
+
+a[a.length] = "craftsman";
+b[b.length] = "řemeslník";
+
+a[a.length] = "awarded";
+b[b.length] = "oceněný";
+
+a[a.length] = "occupied";
+b[b.length] = "obsazený";
+
+a[a.length] = "thus";
+b[b.length] = "tedy";
+
+a[a.length] = "further";
+b[b.length] = "dále";
+
+a[a.length] = "consequently";
+b[b.length] = "tudíž";
+
+a[a.length] = "therefore";
+b[b.length] = "proto";
+
+a[a.length] = "universe";
+b[b.length] = "vesmír";
+
+a[a.length] = "ultimate end";
+b[b.length] = "definitivní konec";
+
+a[a.length] = "chief";
+b[b.length] = "vedoucí";
+
+a[a.length] = "sublime";
+b[b.length] = "vznešený";
+
+a[a.length] = "flesh";
+b[b.length] = "maso";
+
+a[a.length] = "declare";
+b[b.length] = "prohlásit";
+
+a[a.length] = "moreover";
+b[b.length] = "navíc";
+
+a[a.length] = "defines";
+b[b.length] = "definuje";
+
+a[a.length] = "namely";
+b[b.length] = "jmenovitě";
+
+a[a.length] = "pursue";
+b[b.length] = "pronásledovat";
+
+a[a.length] = "refute";
+b[b.length] = "vyvrátit";
+
+a[a.length] = "contrary";
+b[b.length] = "naopak";
+
+a[a.length] = "falsehood";
+b[b.length] = "lež";
+
+a[a.length] = "twofold";
+b[b.length] = "dvojí";
+
+a[a.length] = "publish";
+b[b.length] = "zveřejnit";
+
+a[a.length] = "piety";
+b[b.length] = "zbožnost";
+
+a[a.length] = "conclusion";
+b[b.length] = "závěr";
+
+
+a[a.length] = "shall";
+b[b.length] = "bude";
+
+a[a.length] = "impiety";
+b[b.length] = "bezbožnost";
+
+a[a.length] = "naming";
+b[b.length] = "pojmenování";
+
+a[a.length] = "hence";
+b[b.length] = "proto";
+
+a[a.length] = "conceive";
+b[b.length] = "početí";
+
+a[a.length] = "reckon";
+b[b.length] = "počítat";
+
+a[a.length] = "direct";
+b[b.length] = "řídit";
+
+a[a.length] = "equestrian";
+b[b.length] = "jezdecký";
+
+a[a.length] = "appliance";
+b[b.length] = "přístroj";
+
+a[a.length] = "awarded";
+b[b.length] = "oceněný";
+
+a[a.length] = "occupation";
+b[b.length] = "obsazení";
+
+a[a.length] = "consider";
+b[b.length] = "zvážit";
+
+a[a.length] = "thus";
+b[b.length] = "tím pádem";
+
+a[a.length] = "twofold";
+b[b.length] = "dvojí";
+
+a[a.length] = "contrary";
+b[b.length] = "naopak";
+
+a[a.length] = "opposed";
+b[b.length] = "protichůdný";
+
+a[a.length] = "whereas";
+b[b.length] = "zatímco";
+
+a[a.length] = "reserved";
+b[b.length] = "rezervováno";
+
+a[a.length] = "therefore";
+b[b.length] = "proto";
+
+a[a.length] = "consequently";
+b[b.length] = "tedy";
+
+a[a.length] = "principle";
+b[b.length] = "zásada";
+
+a[a.length] = "pursue";
+b[b.length] = "pronásledovat";
+
+a[a.length] = "comradery";
+b[b.length] = "kamarádství";
+
+a[a.length] = "speak";
+b[b.length] = "mluvit";
+
+a[a.length] = "signified";
+b[b.length] = "znamenalo";
+
+a[a.length] = "denoted";
+b[b.length] = "označené";
+
+a[a.length] = "quote";
+b[b.length] = "citát";
+
+a[a.length] = "receives";
+b[b.length] = "dostává";
+
+a[a.length] = "pursuit";
+b[b.length] = "pronásledovat";
+
+a[a.length] = "profitable";
+b[b.length] = "ziskový";
+
+a[a.length] = "denotes";
+b[b.length] = "označuje";
+
+a[a.length] = "unites";
+b[b.length] = "sjednocuje";
+
+a[a.length] = "unfailing";
+b[b.length] = "neselhávající";
+
+
+a[a.length] = "obtain";
+b[b.length] = "získat";
+
+a[a.length] = "brought";
+b[b.length] = "přinesl";
+
+a[a.length] = "everlasting";
+b[b.length] = "věčný";
+
+a[a.length] = "companionship";
+b[b.length] = "společnost";
+
+a[a.length] = "bitterness";
+b[b.length] = "hořkost";
+
+a[a.length] = "desire";
+b[b.length] = "touha";
+
+a[a.length] = "gladness";
+b[b.length] = "radost";
+
+a[a.length] = "confidence";
+b[b.length] = "důvěra";
+
+a[a.length] = "surpasses";
+b[b.length] = "převyšuje";
+
+a[a.length] = "kindness";
+b[b.length] = "laskavost";
+
+a[a.length] = "purpose";
+b[b.length] = "účel";
+
+a[a.length] = "professes";
+b[b.length] = "vyznává";
+
+a[a.length] = "weeding";
+b[b.length] = "plení";
+
+a[a.length] = "acknowledge";
+b[b.length] = "potvrdit";
+
+a[a.length] = "owe";
+b[b.length] = "dlužím";
+
+a[a.length] = "thought";
+b[b.length] = "myslel";
+
+a[a.length] = "sacrilegious";
+b[b.length] = "svatokrádežný";
+
+a[a.length] = "confute";
+b[b.length] = "vyvrátit";
+
+a[a.length] = "heathens";
+b[b.length] = "pohané";
+
+a[a.length] = "converse";
+b[b.length] = "konverzovat";
+
+a[a.length] = "convicted";
+b[b.length] = "odsouzený";
+
+a[a.length] = "recourse";
+b[b.length] = "útočiště";
+
+a[a.length] = "deficient";
+b[b.length] = "nedostatečný";
+
+a[a.length] = "deficient";
+b[b.length] = "nedostatečný";
+
+a[a.length] = "excluded";
+b[b.length] = "vyloučeno";
+
+a[a.length] = "therebay";
+b[b.length] = "tím";
+
+a[a.length] = "demonstratable";
+b[b.length] = "prokazatelné";
+
+a[a.length] = "it may be";
+b[b.length] = "může to být";
+
+a[a.length] = "its";
+b[b.length] = "své";
+
+a[a.length] = "noble";
+b[b.length] = "ušlechtilý";
+
+a[a.length] = "genera";
+b[b.length] = "rody";
+
+a[a.length] = "genus";
+b[b.length] = "rod";
+
+a[a.length] = "that";
+b[b.length] = "že";
+
+a[a.length] = "those";
+b[b.length] = "ty";
+
+a[a.length] = "argued";
+b[b.length] = "argumentoval";
+
+a[a.length] = "matter";
+b[b.length] = "hmota";
+
+a[a.length] = "likeness";
+b[b.length] = "podobenství";
+
+a[a.length] = "convince";
+b[b.length] = "přesvědčit";
+
+a[a.length] = "neither";
+b[b.length] = "ani ne";
+
+a[a.length] = "hold";
+b[b.length] = "držet";
+
+a[a.length] = "capability";
+b[b.length] = "schopnost";
+
+a[a.length] = "for instance";
+b[b.length] = "například";
+
+a[a.length] = "demonstratively";
+b[b.length] = "názorně";
+
+a[a.length] = "comprehends";
+b[b.length] = "chápe";
+
+a[a.length] = "insofar";
+b[b.length] = "pokud jde o";
+
+a[a.length] = "originates";
+b[b.length] = "pochází";
+
+a[a.length] = "except";
+b[b.length] = "kromně";
+
+a[a.length] = "gathered";
+b[b.length] = "shromážděné";
+
+a[a.length] = "effects";
+b[b.length] = "účinky";
+
+a[a.length] = "unequal";
+b[b.length] = "nerovný";
+
+a[a.length] = "sensible";
+b[b.length] = "rozumný";
+
+a[a.length] = "perceives";
+b[b.length] = "vnímá";
+
+a[a.length] = "subtlety";
+b[b.length] = "jemnost";
+
+a[a.length] = "grasp";
+b[b.length] = "uchopit";
+
+a[a.length] = "instance";
+b[b.length] = "příklad";
+
+a[a.length] = "yokel";
+b[b.length] = "křupan";
+
+a[a.length] = "utterly";
+b[b.length] = "naprosto";
+
+a[a.length] = "mounts";
+b[b.length] = "montuje";
+
+a[a.length] = "mount";
+b[b.length] = "hora";
+
+a[a.length] = "fool";
+b[b.length] = "blázen";
+
+a[a.length] = "insane";
+b[b.length] = "šílený";
+
+a[a.length] = "assertion";
+b[b.length] = "tvrzení";
+
+a[a.length] = "exceedingly";
+b[b.length] = "mimořádně";
+
+a[a.length] = "abundantly";
+b[b.length] = "hojně";
+
+a[a.length] = "deficient";
+b[b.length] = "nedostatečný";
+
+a[a.length] = "primarly";
+b[b.length] = "především";
+
+a[a.length] = "writ";
+b[b.length] = "soudní příkaz";
+
+a[a.length] = "behold";
+b[b.length] = "spatřit";
+
+a[a.length] = "tedious";
+b[b.length] = "zdlouhavý";
+
+a[a.length] = "avoid";
+b[b.length] = "vyhýbat se";
+
+
+a[a.length] = "compelling";
+b[b.length] = "přesvědčivé";
+
+
+a[a.length] = "gap";
+b[b.length] = "mezera";
+
+
+a[a.length] = "regard";
+b[b.length] = "považovat";
+
+a[a.length] = "lest";
+b[b.length] = "jinak";
+
+
+a[a.length] = "useless";
+b[b.length] = "zbytečný";
+
+
+a[a.length] = "unsuitable";
+b[b.length] = "nevhodný";
+
+
+a[a.length] = "society";
+b[b.length] = "společnost";
+
+
+a[a.length] = "hinder";
+b[b.length] = "překážet";
+
+
+a[a.length] = "diligent";
+b[b.length] = "pilný";
+
+
+a[a.length] = "account";
+b[b.length] = "účet";
+
+
+a[a.length] = "effort";
+b[b.length] = "snaha";
+
+
+a[a.length] = "gathering";
+b[b.length] = "shromáždění";
+
+
+a[a.length] = "household";
+b[b.length] = "domácnost";
+
+
+a[a.length] = "affairs";
+b[b.length] = "záležitosti";
+
+
+a[a.length] = "devote";
+b[b.length] = "věnovat";
+
+
+a[a.length] = "conduct";
+b[b.length] = "chování";
+
+
+a[a.length] = "leisure";
+b[b.length] = "volný čas";
+
+
+a[a.length] = "profound";
+b[b.length] = "hluboký";
+
+
+a[a.length] = "beforehand";
+b[b.length] = "předem";
+
+
+a[a.length] = "tossed about";
+b[b.length] = "házel kolem";
+
+
+a[a.length] = "prudence";
+b[b.length] = "opatrnost";
+
+
+a[a.length] = "inconvenience";
+b[b.length] = "nepohodlí";
+
+
+a[a.length] = "judgement";
+b[b.length] = "rozsudek";
+
+
+a[a.length] = "admixture";
+b[b.length] = "přísada";
+
+
+a[a.length] = "phantasm";
+b[b.length] = "přelud";
+
+
+a[a.length] = "clemency";
+b[b.length] = "shovívavost";
+
+
+a[a.length] = "futility";
+b[b.length] = "marnost";
+
